2d0924f300a311fdb0df2fffe2629e8962bc2622
monzalo14 <monzalo14@gmail.com>
2017-04-03 14:32:01 -0500

    ingest de claves salariales

---
+++ pipelines/ingest/bash_scripts/claves_salariales.sh
@@ -0,0 +1,17 @@
+###############
+# Claves salariales para discriminación de funcionarios
+###############
+
+echo "Descarga de Tabla de Remuneraciones en la APF"
+
+wget -q -O-  'http://portaltransparencia.gob.mx/pot/repoServlet?archivo=puestoAPF.zip'| \
+zcat| sed -E 's/(^\=|",\=")/,/g;s/("|^ | $|^\,)//g;s/\s+/ /g;s/^\,//g;s/,$//g;'| sed -n '1!p' | \
+csvcut -c 1,2,3,7| \
+sed '1s/.*/institucion,clave_puesto,nombre_puesto,sueldo_puesto/g'| \
+PGOPTIONS="--search_path=raw" csvsql --db postgresql://compranet:compranetitam@compranetdb.cwioodotgi4s.us-west-2.rds.amazonaws.com/compranetdb --insert --table claves_salariales 
+
+# - 's/(^\=|",\=")/,/g' sustituye globalmente UN '=' o ',=' por ','
+# - 's/("|^ | $|^\,)//g' sustituye globalmente UN '"' o ' ' (al principio) o ' ' (al final) por ''
+# - 's/\s+/ /g' sustituye globalmente cualquier caracter de espacio (UNO O MÁS tabs, espacios, etc.) por ' '
+# - 's/^\,//g;' sustituye globalmente UN ',' al inicio de la línea por ''
+# - 's/,$//g' sustituye globalmente UN ',' al final de la línea por ''
