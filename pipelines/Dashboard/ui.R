026cd1b44ca90a7ecf92de09b2c3de227e1eebae
taguerram <taguerram@gmail.com>
2017-05-16 00:35:05 -0500

    graph stuff

--- pipelines/Dashboard/ui.R
+++ pipelines/Dashboard/ui.R
@@ -11,32 +11,24 @@ shinyUI(fluidPage(
     tabsetPanel(
           
       tabPanel('Dependencias', 
-               column(7, DT::dataTableOutput('tabla_dependencia'))
-               #,
-               #column(5, plotOutput('grafica_dependencia', height = 500))
-               ),
+               column(7, DT::dataTableOutput('tabla_dependencia')),
+               column(5, plotOutput('grafica_dependencia', height = 500))),
           
       tabPanel('Unidades Compradoras', 
-               column(7, DT::dataTableOutput('tabla_unidades'))
-               #,
-               #column(5, plotOutput('grafica_unidades', height = 500))
-               ),
+               column(7, DT::dataTableOutput('tabla_unidades')),
+               column(5, plotOutput('grafica_unidades', height = 500))),
           
       tabPanel('Compras', 
-               column(6, DT::dataTableOutput('tabla_compras'))
-               #,
-               #column(6, plotOutput('grafica_compras', height = 500))
-               ),
+               column(6, DT::dataTableOutput('tabla_compras')),
+               column(6, plotOutput('grafica_compras', height = 500))),
       
-      #tabPanel('Funcionarios', 
-      #         column(7, visNetworkOutput('rel_funcionario_proveedor'))),
+      tabPanel('Funcionarios', 
+               column(7, tableOutput('rel_funcionario_proveedor'))),
       
       tabPanel('Proveedores', 
-               column(7, DT::dataTableOutput('tabla_proveedores'))
-               #,
-               #column(5, plotOutput('grafica_proveedores', height = 500)),
-               #column(7, visNetworkOutput('rel_proveedor_funcionario'))
-               )
+               column(7, DT::dataTableOutput('tabla_proveedores')),
+               column(5, plotOutput('grafica_proveedores', height = 500)))
+               #column(7, visNetworkOutput('rel_proveedor_funcionario')))
       
 
   )
